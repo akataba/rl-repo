9703209281d00b630966951464d136233e709d3f
haknamu@gmail.com <haknamu@gmail.com>
2023-06-03 13:43:08 -0400

    Improve switching noiseless and noisy (commented)

--- examples/train_rllibmodel.py
+++ examples/train_rllibmodel.py
@@ -1,7 +1,19 @@
+import sys
+sys.path.append('../src')
+
 from relaqs.api.training import TrainRLLib
-from relaqs.environments.gate_synth_env_rllib import GateSynthEnvRLlib
+from relaqs.environments.gate_synth_env_rllib import * #import all three below
+#from relaqs.environments.gate_synth_env_rllib import GateSynthEnvRLlib
+#from relaqs.environments.gate_synth_env_rllib import GateSynthEnvRLlibNoiseless
+#from relaqs.environments.gate_synth_env_rllib import GateSynthEnvRLlibNoisy
+
 from ray.rllib.algorithms.ddpg import DDPGConfig
 
 
-trainer = TrainRLLib(DDPGConfig, GateSynthEnvRLlib, episodes=3)
+
+# Choose the right one
+#trainer = TrainRLLib(DDPGConfig, GateSynthEnvRLlib, episodes=300)
+#trainer = TrainRLLib(DDPGConfig, GateSynthEnvRLlibNoiseless, episodes=300)
+trainer = TrainRLLib(DDPGConfig, GateSynthEnvRLlibNoisy, episodes=300)
+
 trainer.train_model()
