7da1a1f25c47fccb0001e3950e4c1507b3bfac1d
Collin Farquhar <collin.farquhar@zapatacomputing.com>
2023-06-05 11:51:45 -0700

    script used to generate results and save model

---
+++ scripts/run_and_save.py
@@ -0,0 +1,28 @@
+import ray
+from ray.rllib.algorithms.ddpg import DDPGConfig
+from ray.tune.registry import register_env
+from relaqs.environments.gate_synth_env_rllib import GateSynthEnvRLlib
+import numpy as np
+
+def env_creator(config):
+    return GateSynthEnvRLlib(config)
+
+ray.init()
+register_env("my_env", env_creator)
+alg_config = DDPGConfig()
+alg_config.framework("torch")
+alg_config.environment("my_env", env_config=GateSynthEnvRLlib.get_default_env_config())
+alg_config.rollouts(batch_mode="complete_episodes")
+alg_config.train_batch_size = 1
+alg_config.actor_lr = 1e-4
+alg_config.critic_lr = 1e-4
+alg_config.num_steps_sampled_before_learning_starts = 100
+alg = alg_config.build()
+print(type(alg))
+
+for _ in range(100):
+    result = alg.train()
+
+path_to_checkpoint = alg.save()
+
+print("path_to_checkpoint", path_to_checkpoint)
