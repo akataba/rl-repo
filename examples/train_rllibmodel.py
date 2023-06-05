625639433cc4823037b7f3e1eecdcabc161e24f9
haknamu@gmail.com <haknamu@gmail.com>
2023-06-05 05:47:25 -0400

    GateSynthEnvRLlibNoiseless - > GateSynthEnvRLlib

--- examples/train_rllibmodel.py
+++ examples/train_rllibmodel.py
@@ -3,21 +3,18 @@ sys.path.append('../src')
 
 from relaqs.api.training import TrainRLLib
 
-#from relaqs.environments.gate_synth_env_rllib import * #import all three below
-#from relaqs.environments.gate_synth_env_rllib import GateSynthEnvRLlib
-#from relaqs.environments.gate_synth_env_rllib import GateSynthEnvRLlibNoiseless
+#from relaqs.environments.gate_synth_env_rllib import * #import all 
+from relaqs.environments.gate_synth_env_rllib import GateSynthEnvRLlib
 #from relaqs.environments.gate_synth_env_rllib import GateSynthEnvRLlibNoisy
-
-from relaqs.environments.gate_synth_env_rllib_Haar import GateSynthEnvRLlibHaarNoiseless
+#from relaqs.environments.gate_synth_env_rllib_Haar import GateSynthEnvRLlibHaarNoiseless
 
 from ray.rllib.algorithms.ddpg import DDPGConfig
 
 
 
 # Choose the right one
-#trainer = TrainRLLib(DDPGConfig, GateSynthEnvRLlib, episodes=300)
-#trainer = TrainRLLib(DDPGConfig, GateSynthEnvRLlibNoiseless, episodes=300)
+trainer = TrainRLLib(DDPGConfig, GateSynthEnvRLlib, episodes=300)
 #trainer = TrainRLLib(DDPGConfig, GateSynthEnvRLlibNoisy, episodes=300)
-trainer = TrainRLLib(DDPGConfig, GateSynthEnvRLlibHaarNoiseless, episodes=300)
+#trainer = TrainRLLib(DDPGConfig, GateSynthEnvRLlibHaarNoiseless, episodes=300)
 
 trainer.train_model()
