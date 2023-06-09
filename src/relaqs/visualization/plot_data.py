fee96bf616b34beb95e2705def3f90d44e90cee3
haknamu@gmail.com <haknamu@gmail.com>
2023-06-09 16:36:15 -0400

    saving actions

--- src/relaqs/visualization/plot_data.py
+++ src/relaqs/visualization/plot_data.py
@@ -2,11 +2,13 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 
-fig, axs = plt.subplots(1, 3, figsize=(12, 4))
+fig, axs = plt.subplots(1, 5, figsize=(20, 4))
 fig.suptitle('RLlib')
 axs[0].set_title('Fidelity')
 axs[1].set_title('1 - Fidelity (log scale)')
 axs[2].set_title('Reward')
+axs[3].set_title('magnitude')
+axs[4].set_title('phase')
 
 def plot(file_dir):
     # Find the latest file
@@ -20,7 +22,7 @@ def plot(file_dir):
                 max_file_path = os.path.join(file_dir, file_name)
 
     # Read the data from the latest file
-    fidelity, reward = [], []
+    fidelity, reward, mag, phase = [], [], [], []
     if max_file_path:
         with open(max_file_path, "r") as file:
             lines = file.readlines()
@@ -28,16 +30,22 @@ def plot(file_dir):
                 data = line.strip().split(",")
                 fidelity.append(float(data[0]))
                 reward.append(float(data[1]))
+                mag.append(float(data[2]))
+                phase.append(float(data[3]))
 
     x = np.arange(len(fidelity))
 
     axs[0].plot(x, fidelity, c='blue')
     axs[1].semilogy(x, [1 - ii for ii in fidelity], c='red')  # Set y axis to log scale
     axs[2].plot(x, reward, c='green')
+    axs[3].plot(x, mag, c='purple')
+    axs[4].plot(x, phase, c='orange')
 
     axs[0].set_xlim(0, len(fidelity))
     axs[1].set_xlim(0, len(fidelity))
     axs[2].set_xlim(0, len(reward))
+    axs[3].set_xlim(0, len(mag))
+    axs[4].set_xlim(0, len(phase))
 
     plt.show()
 
