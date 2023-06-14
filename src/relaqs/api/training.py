f3b371f02688b876bb5e620d6a3ec4b2422f87f1
Marc Davis <mgd@mit.edu>
2023-06-13 21:18:04 -0400

    made sampling multithreaded, seemingly at the expense of GPU training

--- src/relaqs/api/training.py
+++ src/relaqs/api/training.py
@@ -9,6 +9,8 @@ class TrainRLLib:
         register_env("my_env", self.env_creator)
         self.alg_config = rllibmodel()
         self.alg_config.framework(framework_name)
+        self.alg_config.resources(num_gpus=int(ray.available_resources()["GPU"]))
+        self.alg_config.rollouts(num_rollout_workers=int(ray.available_resources()["CPU"]))
         self.alg_config.environment("my_env", env_config=env.get_default_env_config())
         self.alg = self.alg_config.build()
         self.episodes = episodes
@@ -17,7 +19,7 @@ class TrainRLLib:
         return self.env(config)
 
     def train_model(self):
-        for _ in range(self.episodes):
+        for i in range(self.episodes):
             result = self.alg.train()
 
 
