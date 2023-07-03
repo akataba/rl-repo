5b48553a53ac91e12de727c9904e46a132794532
Collin Farquhar <collin.farquhar@zapatacomputing.com>
2023-07-03 15:35:17 -0600

    add directory for quantum data

--- src/relaqs/__init__.py
+++ src/relaqs/__init__.py
@@ -1,5 +1,6 @@
 import os
 
-path_of_current_file = os.path.dirname(os.path.realpath(__file__))
-path_to_relaqs_root = path_of_current_file.partition("src")[0]
-RESULTS_DIR = path_to_relaqs_root + "results/"
+path_to_relaqs_root = os.path.dirname(os.path.realpath(__file__))
+path_relaqs_parent = path_to_relaqs_root.partition("src")[0]
+QUANTUM_NOISE_DATA_DIR = path_to_relaqs_root + "/quantum_noise_data/"
+RESULTS_DIR = path_relaqs_parent + "results/"
