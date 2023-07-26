faa7f52641b4b66d840accb443bc12a48954b48c
Collin Farquhar <collin.farquhar@zapatacomputing.com>
2023-07-26 12:04:23 -0600

    fidelity computations in utils

--- src/relaqs/api/__init__.py
+++ src/relaqs/api/__init__.py
@@ -1,1 +1,3 @@
 from .training import TrainRLLib
+from .gates import Gate
+from .utils import gate_fidelity, dm_fidelity
---
+++ src/relaqs/api/utils.py
@@ -0,0 +1,8 @@
+import numpy as np
+from scipy.linalg import sqrtm
+gate_fidelity = lambda U, V: float(np.abs(np.trace(U @ V))) / (U.shape[0])
+
+def dm_fidelity(rho, sigma):
+    assert np.trace(sqrtm(sqrtm(rho) @ sigma @ sqrtm(rho))).imag < 1e-8, f"Non-negligable imaginary component {np.trace(sqrtm(sqrtm(rho) @ sigma @ sqrtm(rho))).imag}"
+    #return np.abs(np.trace(sqrtm(sqrtm(rho) @ sigma @ sqrtm(rho))))**2
+    return np.trace(sqrtm(sqrtm(rho) @ sigma @ sqrtm(rho))).real**2
