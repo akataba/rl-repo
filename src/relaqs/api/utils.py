616d249c7a8b3295149ed658016d92eae4e6c961
Collin Farquhar <collin.farquhar@zapatacomputing.com>
2023-07-26 12:57:52 -0600

    fix gate fidelity

--- src/relaqs/api/utils.py
+++ src/relaqs/api/utils.py
@@ -1,6 +1,6 @@
 import numpy as np
 from scipy.linalg import sqrtm
-gate_fidelity = lambda U, V: float(np.abs(np.trace(U @ V))) / (U.shape[0])
+gate_fidelity = lambda U, V: float(np.abs(np.trace(U.conjugate().transpose() @ V))) / (U.shape[0])
 
 def dm_fidelity(rho, sigma):
     assert np.trace(sqrtm(sqrtm(rho) @ sigma @ sqrtm(rho))).imag < 1e-8, f"Non-negligable imaginary component {np.trace(sqrtm(sqrtm(rho) @ sigma @ sqrtm(rho))).imag}"
