686708930969d7f92d5f0848b7436839262776f2
Collin Farquhar <collin.farquhar@zapatacomputing.com>
2023-08-25 14:51:52 -0600

    experiment script using hpt 1

--- scripts/learning_other_gates_noiseless.py
+++ scripts/learning_other_gates_noiseless.py
@@ -27,20 +27,33 @@ def run(n_training_iterations=1, save=True, plot=True):
     alg_config.environment("my_env", env_config=env_config)
     #alg_config.environment(GateSynthEnvRLlibHaar, env_config=GateSynthEnvRLlibHaar.get_default_env_config())
 
+    # ---> previous settings <---
     alg_config.rollouts(batch_mode="complete_episodes")
 
-    ### working 1-3 sets
-    alg_config.actor_lr = 4e-5
-    alg_config.critic_lr = 5e-4
+    # ### working 1-3 sets
+    # alg_config.actor_lr = 4e-5
+    # alg_config.critic_lr = 5e-4
 
-    alg_config.actor_hidden_activation = "relu"
-    alg_config.critic_hidden_activation = "relu"
-    alg_config.num_steps_sampled_before_learning_starts = 1000
-    alg_config.actor_hiddens = [30,30,30]
-    alg_config.exploration_config["scale_timesteps"] = 10000
+    # alg_config.actor_hidden_activation = "relu"
+    # alg_config.critic_hidden_activation = "relu"
+    # alg_config.num_steps_sampled_before_learning_starts = 1000
+    # alg_config.actor_hiddens = [30,30,30]
+    # alg_config.exploration_config["scale_timesteps"] = 10000
+    # ---------------------------
+
+    # ---> from hyperparameter tuning <---
+    alg_config.actor_lr = 6e-5
+    alg_config.critic_lr = 8e-5
+
+    # alg_config.actor_hidden_activation = "relu"
+    # alg_config.critic_hidden_activation = "relu"
+    #alg_config.num_steps_sampled_before_learning_starts = config["sampled_before_learning"]
+    #alg_config.exploration_config["scale_timesteps"] = config["scale_timesteps"]
+    alg_config.actor_hiddens = [50] * 10
+    alg_config.critic_hiddens = [300] * 10
+    # ---------------------------------------------------------------------
 
     alg = alg_config.build()
-    # ---------------------------------------------------------------------
 
     # ---------------------> Train Agent <-------------------------
     for _ in range(n_training_iterations):
@@ -63,7 +76,7 @@ def run(n_training_iterations=1, save=True, plot=True):
     # --------------------------------------------------------------
 
 if __name__ == "__main__":
-    n_training_iterations = 100 
+    n_training_iterations = 300
     save = True
     plot = True
     run(n_training_iterations, save, plot)
