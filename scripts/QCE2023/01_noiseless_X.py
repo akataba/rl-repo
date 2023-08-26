69833896d8007aa7c78ca93f68536c5461c8ee81
haknamu@gmail.com <haknamu@gmail.com>
2023-08-26 17:59:47 -0400

    QCE2023 01_noiseless_x

---
+++ scripts/QCE2023/01_noiseless_X.py
@@ -0,0 +1,93 @@
+import sys
+sys.path.append('./src/')
+
+import ray
+from ray.rllib.algorithms.ddpg import DDPGConfig
+from ray.tune.registry import register_env
+from relaqs.environments.gate_synth_env_rllib_Haar import GateSynthEnvRLlibHaar
+from relaqs.save_results import SaveResults
+from relaqs.plot_data import plot_data
+from relaqs import RESULTS_DIR
+
+import datetime
+
+def env_creator(config):
+    return GateSynthEnvRLlibHaar(config)
+
+def save_grad_to_file(resultdict):
+    try:
+        policydict = resultdict["default_policy"]
+        stats = policydict["learner_stats"]
+        grad_gnorm = stats["grad_gnorm"]
+        with open("gradfile", "a") as f:
+            f.write(f"{grad_gnorm}\n")
+    except KeyError:
+        print(f"Failed to extract grad_gnorm from: {resultdict}")
+
+def inject_logging(alg, logging_func):
+    og_ts = alg.training_step
+    def new_training_step():
+        result = og_ts()
+        # do logging here
+        logging_func(result)
+        return result
+    alg.training_step = new_training_step
+
+def run(n_training_iterations=1, save=True, plot=True):
+    ray.init()
+    try:
+        register_env("my_env", env_creator)
+
+        # ---------------------> Configure algorithm and Environment <-------------------------
+        alg_config = DDPGConfig()
+        alg_config.framework("torch")
+        alg_config.environment("my_env", env_config=GateSynthEnvRLlibHaar.get_default_env_config())
+
+        alg_config.rollouts(batch_mode="complete_episodes")
+        alg_config.train_batch_size = GateSynthEnvRLlibHaar.get_default_env_config()["steps_per_Haar"]
+
+        ### working 1-3 sets
+        alg_config.actor_lr = 4e-5
+        alg_config.critic_lr = 5e-4
+
+        alg_config.actor_hidden_activation = "relu"
+        alg_config.critic_hidden_activation = "relu"
+        alg_config.num_steps_sampled_before_learning_starts = 500
+        alg_config.actor_hiddens = [30,30,30]
+        alg_config.exploration_config["scale_timesteps"] = 1000
+        print(alg_config.algo_class)
+        print(alg_config["framework"])
+
+        alg = alg_config.build()
+        inject_logging(alg, save_grad_to_file)
+        # ---------------------------------------------------------------------
+        list_of_results = []
+        # ---------------------> Train Agent <-------------------------
+        for _ in range(n_training_iterations):
+            result = alg.train()
+            list_of_results.append(result['hist_stats'])
+        # -------------------------------------------------------------
+
+        # ---------------------> Save Results <-------------------------
+        if save is True:
+            env = alg.workers.local_worker().env
+            sr = SaveResults(env, alg, results=list_of_results, save_path = RESULTS_DIR + "QCE2023/"+"01_noiseless_X" + datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S/"))
+            save_dir = sr.save_results()
+            print("Results saved to:", save_dir)
+        # --------------------------------------------------------------
+
+        # ---------------------> Plot Data <-------------------------
+        if plot is True:
+            assert save is True, "If plot=True, then save must also be set to True"
+            plot_data(save_dir, episode_length=alg._episode_history[0].episode_length)
+            print("Plots Created")
+        # --------------------------------------------------------------
+    finally:
+        ray.shutdown()
+
+if __name__ == "__main__":
+    n_training_iterations = 10
+    save = True
+    plot = True
+    run(n_training_iterations, save, plot)
+    
