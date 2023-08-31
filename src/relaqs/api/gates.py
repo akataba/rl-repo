ad31d76937f7543b58529f7eb781988b5fa257e5
Collin Farquhar <collin.farquhar@zapatacomputing.com>
2023-08-30 21:31:44 -0400

    method to get random su2

--- src/relaqs/api/gates.py
+++ src/relaqs/api/gates.py
@@ -18,3 +18,22 @@ class Gate:
     S = np.exp(-1j * Z * np.pi/4)
 
     X_pi_4 = np.exp(-1j * X * np.pi/4)
+
+def get_random_su2():
+    """
+    http://home.lu.lv/~sd20008/papers/essays/Random%20unitary%20[paper].pdf eqn. (19)
+    """  
+    alpha = np.random.uniform(low=0, high=2*np.pi)
+    psi = np.random.uniform(low=0, high=2*np.pi)
+    chi = np.random.uniform(low=0, high=2*np.pi)
+    zeta = np.random.randn()
+    phi = np.arcsin(np.sqrt(zeta))
+
+    U = np.zeros((2, 2))
+    U[0][0] = np.exp(1j * psi) * np.cos(phi)
+    U[0][1] = np.exp(1j * chi) * np.sin(phi)
+    U[1][0] = -1 * np.exp(-1j * chi) * np.sin(phi)
+    U[1][1] = np.exp(-1j * psi) * np.cos(phi)
+    U = np.exp(1j * alpha) * U # apply global phase
+
+    return U
