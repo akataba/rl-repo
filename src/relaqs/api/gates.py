1d06a50c8b56faa3dc2dd3c1ff84fcf3b47a12fe
Collin Farquhar <collin.farquhar@zapatacomputing.com>
2023-09-06 11:58:04 -0600

    methods for uniform random U(2) and random SU(2)

--- src/relaqs/api/gates.py
+++ src/relaqs/api/gates.py
@@ -19,39 +19,33 @@ class Gate:
 
     X_pi_4 = np.exp(-1j * X * np.pi/4)
 
-# Does not give unitary, not sure why
-# def get_random_su2():
-#     """
-#     http://home.lu.lv/~sd20008/papers/essays/Random%20unitary%20[paper].pdf eqn. (19)
-#     """  
-#     alpha = np.random.uniform(low=0, high=2*np.pi)
-#     psi = np.random.uniform(low=0, high=2*np.pi)
-#     chi = np.random.uniform(low=0, high=2*np.pi)
-#     zeta = np.random.uniform(low=0, high=1)
-#     phi = np.arcsin(np.sqrt(zeta))
-
-#     U = np.zeros((2, 2), dtype=np.complex64)
-#     U[0][0] = np.exp(1j * psi) * np.cos(phi)
-#     U[0][1] = np.exp(1j * chi) * np.sin(phi)
-#     U[1][0] = -1 * np.exp(-1j * chi) * np.sin(phi)
-#     U[1][1] = np.exp(-1j * psi) * np.cos(phi)
-#     U = np.exp(1j * alpha) * U # apply global phase
-
-#     deterimant = np.linalg.det(U)
-#     print(deterimant)
-#     U = U / np.linalg.det(U)
-
-#     assert np.allclose(np.eye(2), U @ U.T.conjugate())
-
-#     return U
-
-def get_random_su2():
-    """ Using QR decomposition procedure from: https://case.edu/artsci/math/esmeckes/Meckes_SAMSI_Lecture2.pdf """
+def get_random_u2():
+    """ 
+    Returns a uniform random of U(2).
+    Uses QR decomposition procedure from: https://case.edu/artsci/math/esmeckes/Meckes_SAMSI_Lecture2.pdf 
+    """
     complex_rvs = [np.random.normal(0, 1/np.sqrt(2)) + 1j * np.random.normal(0, 1/np.sqrt(2)) for _ in range(4)] # standard complex gaussian random variables
     A = np.array(complex_rvs).reshape(2, 2)
     U, _ = np.linalg.qr(A)
     assert np.allclose(np.eye(2), U @ U.T.conjugate()) # Check unitarity
+    #print("Det", np.linalg.det(U))
     return U
 
+def get_random_su2():
+  """
+  Returns a random element of SU(2).
+  """
+  theta = np.random.uniform(0, np.pi)
+  phi = np.random.uniform(0, 2 * np.pi)
+  u = np.cos(theta / 2)
+  v = np.sin(theta / 2) * np.exp(1j * phi)
+  return np.array([[u, v], [-np.conjugate(v), u]])
+
 if __name__ == "__main__":
-    U = get_random_su2()
+    u1 = get_random_u2()
+    assert np.allclose(np.eye(2), u1 @ u1.T.conjugate()) # Check unitarity
+    print("determinant of element from U(2)", np.linalg.det(u1))
+
+    u2 = get_random_su2()
+    assert np.allclose(np.eye(2), u2 @ u2.T.conjugate()) # Check unitarity
+    print("determinant of element from SU(2)", np.linalg.det(u2))  
