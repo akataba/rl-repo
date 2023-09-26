84f9a0b554c67a279e3ac3ef460c544f55254b91
Collin Farquhar <collin.farquhar@zapatacomputing.com>
2023-09-26 16:32:21 -0600

    refactor gates as classes with str

--- src/relaqs/api/gates.py
+++ src/relaqs/api/gates.py
@@ -1,4 +1,5 @@
 import numpy as np
+from abc import ABC, abstractmethod
 from scipy.stats import rv_continuous
 
 class sin_prob_dist(rv_continuous):
@@ -6,40 +7,88 @@ class sin_prob_dist(rv_continuous):
         # The 0.5 is so that the distribution is normalized
         return 0.5 * np.sin(theta)
 
-sin_sampler = sin_prob_dist(a=0, b=np.pi)
+class Gate(ABC):
+    def __init__(self):
+        self.sin_sampler = sin_prob_dist(a=0, b=np.pi)
 
-class Gate: 
-    I = np.eye(2)
+    @abstractmethod
+    def __str__(self):
+        pass
 
-    X = np.array([[0, 1],
-                [1, 0]])
+    @abstractmethod
+    def get_matrix(self):
+        pass
 
-    Y = np.array([[0, 1j],
-                [-1j, 0]])
+class I(Gate):
+    def __str__(self):
+        return "I"
+    
+    def get_matrix(self):
+        return np.eye(2)
 
-    Z = np.array([[1, 0],
-                [0, -1]])
+class X(Gate):
+    def __str__(self):
+        return "X"
+    
+    def get_matrix(self):
+        return np.array([[0, 1],
+                         [1, 0]])
 
-    H = 1/np.sqrt(2) * np.array([[1, 1],
-                                [1, -1]])
+class Y(Gate):
+    def __str__(self):
+        return "Y"
+    
+    def get_matrix(self):
+        return np.array([[0, 1j],
+                         [-1j, 0]])
 
-    S = np.exp(-1j * Z * np.pi/4)
+class Z(Gate):
+    def __str__(self):
+        return "Z"
+    
+    def get_matrix(self):
+        return np.array([[1, 0],
+                         [0, -1]])
 
-    X_pi_4 = np.exp(-1j * X * np.pi/4)
+class H(Gate):
+    def __str__(self):
+        return "H"
+    
+    def get_matrix(self):
+        return 1/np.sqrt(2) * np.array([[1, 1],
+                                        [1, -1]])
 
-def get_haar_random_SU2():
-    """
-    Returns a Haar Random element of SU(2).
-    https://pennylane.ai/qml/demos/tutorial_haar_measure
-    """
-    phi = np.random.uniform(low=0, high=2*np.pi)
-    omega = np.random.uniform(low=0, high=2*np.pi)
-    theta = sin_sampler.rvs(size=1)[0]
+class S(Gate):
+    def __str__(self):
+        return "S"
+    
+    def get_matrix(self):
+        return np.exp(-1j * Z().get_matrix() * np.pi/4)
 
-    U = np.zeros((2, 2), dtype=np.complex128)
-    U[0][0] = np.exp(-1j * (phi + omega) / 2) * np.cos(theta / 2)
-    U[0][1] = -1 * np.exp(1j * (phi - omega) / 2) * np.sin(theta / 2)
-    U[1][0] = np.exp(-1j * (phi - omega) / 2) * np.sin(theta / 2)
-    U[1][1] = np.exp(1j * (phi + omega) / 2) * np.cos(theta / 2)
+class X_pi_4(Gate):
+    def __str__(self):
+        return "X_pi_4"
+    
+    def get_matrix(self):
+        return np.exp(-1j * X().get_matrix() * np.pi/4)
 
-    return U
+class RandomSU2(Gate):
+    def __str__(self):
+        return "RandomSU2"
+    
+    def get_matrix(self):
+        """
+        Returns a Haar Random element of SU(2).
+        https://pennylane.ai/qml/demos/tutorial_haar_measure
+        """
+        phi = np.random.uniform(low=0, high=2*np.pi)
+        omega = np.random.uniform(low=0, high=2*np.pi)
+        theta = self.sin_sampler.rvs(size=1)[0]
+
+        U = np.zeros((2, 2), dtype=np.complex128)
+        U[0][0] = np.exp(-1j * (phi + omega) / 2) * np.cos(theta / 2)
+        U[0][1] = -1 * np.exp(1j * (phi - omega) / 2) * np.sin(theta / 2)
+        U[1][0] = np.exp(-1j * (phi - omega) / 2) * np.sin(theta / 2)
+        U[1][1] = np.exp(1j * (phi + omega) / 2) * np.cos(theta / 2)
+
+        return U
