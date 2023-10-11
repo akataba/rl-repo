0863650546636dda1422e324418ce36f5cabaf13
Collin Farquhar <farquhar13@gmail.com>
2023-10-10 22:30:33 -0600

    clean up

--- examples/hyperparameter_tuning.py
+++ examples/hyperparameter_tuning.py
@@ -7,7 +7,7 @@ from ray.tune.registry import register_env
 from relaqs.environments.gate_synth_env_rllib_Haar import GateSynthEnvRLlibHaarNoisy, GateSynthEnvRLlibHaar
 from relaqs.save_results import SaveResults
 from relaqs.plot_data import plot_data
-from relaqs.api.gates import Gate
+import relaqs.api.gates as gates
 
 def env_creator(config):
     #return GateSynthEnvRLlibHaarNoisy(config)
@@ -32,50 +32,20 @@ def run_ray_tune(save=True, plot=True):
     best_avg_fidelity_config = results.get_best_result(metric="fidelity", mode="max", scope="last-50-avg").config
     print("best_fidelity_config", best_fidelity_config)
     print("best_avg_fidelity_config", best_avg_fidelity_config)
-    return
-    alg = run_one_training_cycle(best_fidelity_config, full_output=True)
-    # ---------------------> Save Results <-------------------------
-    if save is True:
-        env = alg.workers.local_worker().env
-        sr = SaveResults(env, alg, save_base_path="./max_fidelity/")
-        save_dir = sr.save_results()
-        print("Results saved to:", save_dir)
-    # --------------------------------------------------------------
-
-    # ---------------------> Plot Data <-------------------------
-    if plot is True:
-        assert save is True, "If plot=True, then save must also be set to True"
-        plot_data(save_dir, episode_length=alg._episode_history[0].episode_length)
-        print("Plots Created")
-    # --------------------------------------------------------------
-
-
-
-
 
 def run_one_training_cycle(config, full_output=False):
     # ---------------------> Configure algorithm and Environment <-------------------------
     alg_config = DDPGConfig()
     alg_config.framework("torch")
     env_config = GateSynthEnvRLlibHaar.get_default_env_config()
-    env_config["U_target"] = Gate.H
+    env_config["U_target"] = gates.H().get_matrix()
     alg_config.environment("my_env", env_config=env_config)
-    #alg_config.environment(GateSynthEnvRLlibHaarNoisy, env_config=GateSynthEnvRLlibHaarNoisy.get_default_env_config())
 
-    #alg_config.rollouts(batch_mode="complete_episodes")
-    #alg_config.train_batch_size = GateSynthEnvRLlibHaar.get_default_env_config()["steps_per_Haar"]
-
-    ### working 1-3 sets
     alg_config.actor_lr = config["actor_lr"]
     alg_config.critic_lr = config["critic_lr"]
 
-    # alg_config.actor_hidden_activation = "relu"
-    # alg_config.critic_hidden_activation = "relu"
-    #alg_config.num_steps_sampled_before_learning_starts = config["sampled_before_learning"]
-    #alg_config.exploration_config["scale_timesteps"] = config["scale_timesteps"]
     alg_config.actor_hiddens = [config["actor_layer_size"]] * config["actor_num_hiddens"]
     alg_config.critic_hiddens = [config["critic_layer_size"]] * config["critic_num_hiddens"]
-
 
     alg = alg_config.build()
     # ---------------------------------------------------------------------
