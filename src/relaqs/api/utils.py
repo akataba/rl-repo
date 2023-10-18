cf9fa89825aef1562c3a010a40128a388a760d02
akataba <henriamaa@gmail.com>
2023-10-18 18:42:42 -0400

    adding functions to do inferencing on trained model

--- src/relaqs/api/utils.py
+++ src/relaqs/api/utils.py
@@ -1,4 +1,6 @@
 import numpy as np
+from ray.rllib.algorithms.algorithm import Algorithm
+
 from scipy.linalg import sqrtm
 gate_fidelity = lambda U, V: float(np.abs(np.trace(U.conjugate().transpose() @ V))) / (U.shape[0])
 
@@ -6,3 +8,38 @@ def dm_fidelity(rho, sigma):
     assert np.trace(sqrtm(sqrtm(rho) @ sigma @ sqrtm(rho))).imag < 1e-8, f"Non-negligable imaginary component {np.trace(sqrtm(sqrtm(rho) @ sigma @ sqrtm(rho))).imag}"
     #return np.abs(np.trace(sqrtm(sqrtm(rho) @ sigma @ sqrtm(rho))))**2
     return np.trace(sqrtm(sqrtm(rho) @ sigma @ sqrtm(rho))).real**2
+
+def do_inferencing(alg, n_episodes_for_inferencing):
+    """
+    alg: The trained model
+    n_episodes_for_inferencing: Number of episodes to do during the training
+    """
+    
+    assert n_episodes_for_inferencing > 0
+    env = alg.workers.local_worker().env
+    obs, info = env.reset()
+    num_episodes = 0
+    episode_reward = 0.0
+    print("Inferencing is starting ....")
+    while num_episodes < n_episodes_for_inferencing:
+        print("episode : ", num_episodes)
+        # Compute an action (`a`).
+        a = alg.compute_single_action(
+            observation=obs,
+            policy_id="default_policy",  # <- default value
+        )
+        # Send the computed action `a` to the env.
+        obs, reward, done, truncated, _ = env.step(a)
+        episode_reward += reward
+        # Is the episode `done`? -> Reset.
+        if done:
+            print(f"Episode done: Total reward = {episode_reward}")
+            obs, info = env.reset()
+            num_episodes += 1
+            episode_reward = 0.0
+    return env, alg
+
+def load_model(path):
+    "path (str): Path to the file usually beginning with the word 'checkpoint' " 
+    loaded_model = Algorithm.from_checkpoint(path)
+    return loaded_model
