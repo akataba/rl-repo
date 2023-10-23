939393ba59d6a869053292ef03cb5448b7b906a9
Marc Davis <mgd@mit.edu>
2023-10-23 03:32:57 -0400

    made the default save dir be based on the installation directory, assuming the package was installed with -e

--- src/relaqs/__init__.py
+++ src/relaqs/__init__.py
@@ -1,6 +1,19 @@
 import os
+import sys
 
-path_to_relaqs_root = os.path.dirname(os.path.realpath(__file__))
-path_relaqs_parent = path_to_relaqs_root.partition("src")[0]
-QUANTUM_NOISE_DATA_DIR = path_to_relaqs_root + "/quantum_noise_data/"
-RESULTS_DIR = path_relaqs_parent + "results/"
+path_to_relaqs_installation = os.path.dirname(os.path.realpath(__file__))
+
+try:
+    QUANTUM_NOISE_DATA_DIR = os.environ["QUANTUM_NOISE_DATA_DIR"]
+except:
+    QUANTUM_NOISE_DATA_DIR = path_to_relaqs_installation + "/quantum_noise_data/"
+
+try:
+    RESULTS_DIR = os.environ["SAVE_BASE_PATH"]
+except:
+    path_to_relaqs_alias_github_root = os.path.abspath(os.path.join(os.path.join(path_to_relaqs_installation, os.pardir), os.pardir))
+    RESULTS_DIR = os.path.join(path_to_relaqs_alias_github_root,"results")
+
+
+
+
