58296c0ab1d6a4b5114b08f852cf3115bf7b44c0
Marc Davis <mgd@mit.edu>
2023-10-23 03:19:00 -0400

    made the default dir be based on sys.argv[0] rather than os.path.realpath of an __init__.py in the installation folder

--- src/relaqs/__init__.py
+++ src/relaqs/__init__.py
@@ -1,6 +1,15 @@
 import os
+import sys
 
-path_to_relaqs_root = os.path.dirname(os.path.realpath(__file__))
-path_relaqs_parent = path_to_relaqs_root.partition("src")[0]
-QUANTUM_NOISE_DATA_DIR = path_to_relaqs_root + "/quantum_noise_data/"
-RESULTS_DIR = path_relaqs_parent + "results/"
+try:
+    RESULTS_DIR = os.environ["SAVE_BASE_PATH"]
+except:
+    path_to_relaqs_script = os.path.dirname(os.path.abspath(sys.argv[0]))
+    path_to_relaqs_root = os.path.abspath(os.path.join(path_to_relaqs_script, os.pardir))
+    RESULTS_DIR = os.path.join(path_to_relaqs_root,"results")
+
+
+path_to_relaqs_installation = os.path.dirname(os.path.realpath(__file__))
+QUANTUM_NOISE_DATA_DIR = path_to_relaqs_installation + "/quantum_noise_data/"
+
+
