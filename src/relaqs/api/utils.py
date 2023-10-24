f8449025e3c56ed6c307f1c062aa3b4b07a342ab
akataba <henriamaa@gmail.com>
2023-10-24 19:17:34 -0400

    modifing do_inferencing to allow for multiple noise parameters

--- src/relaqs/api/utils.py
+++ src/relaqs/api/utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 from ray.rllib.algorithms.algorithm import Algorithm
+from relaqs.quantum_noise_data.get_data import get_month_of_single_qubit_data, get_month_of_all_qubit_data
 
 from scipy.linalg import sqrtm
 gate_fidelity = lambda U, V: float(np.abs(np.trace(U.conjugate().transpose() @ V))) / (U.shape[0])
@@ -8,6 +9,16 @@ def dm_fidelity(rho, sigma):
     assert np.trace(sqrtm(sqrtm(rho) @ sigma @ sqrtm(rho))).imag < 1e-8, f"Non-negligable imaginary component {np.trace(sqrtm(sqrtm(rho) @ sigma @ sqrtm(rho))).imag}"
     #return np.abs(np.trace(sqrtm(sqrtm(rho) @ sigma @ sqrtm(rho))))**2
     return np.trace(sqrtm(sqrtm(rho) @ sigma @ sqrtm(rho))).real**2
+
+def sample_noise_parameters(path_to_file):
+    # ---------------------> Get quantum noise data <-------------------------
+    t1_list, t2_list = get_month_of_all_qubit_data(path_to_file)        #in seconds
+    mean = 0
+    std = 0.03
+    sample_size = 100
+    samples = np.random.normal(mean, std, sample_size)
+    samples_list = samples.tolist()
+    return t1_list, t2_list, samples_list
 
 def do_inferencing(alg, n_episodes_for_inferencing):
     """
@@ -18,6 +29,9 @@ def do_inferencing(alg, n_episodes_for_inferencing):
     assert n_episodes_for_inferencing > 0
     env = alg.workers.local_worker().env
     obs, info = env.reset()
+    t1_list, t2_list, detuning_list = sample_noise_parameters("/Users/amara/Dropbox/Zapata/rl_learn/src/relaqs/quantum_noise_data/april/ibmq_manila_month_is_4.json")
+    env.relaxation_rates_list = [np.reciprocal(t1_list).tolist(), np.reciprocal(t2_list).tolist()]
+    env.delta = detuning_list  
     num_episodes = 0
     episode_reward = 0.0
     print("Inferencing is starting ....")
