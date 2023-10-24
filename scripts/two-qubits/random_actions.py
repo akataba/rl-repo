7a9f25a5a0a8f4320b2df986035fbbbd86f637ca
Collin Farquhar <farquhar13@gmail.com>
2023-10-23 23:09:40 -0600

    random actions numerics

---
+++ scripts/two-qubits/random_actions.py
@@ -0,0 +1,20 @@
+from relaqs.environments.gate_synth_env_rllib_Haar import TwoQubitGateSynth
+from relaqs import RESULTS_DIR
+import numpy as np
+import csv
+
+n_episodes = 100_000 
+
+env = TwoQubitGateSynth(TwoQubitGateSynth.get_default_env_config())
+for _ in range(n_episodes):
+    terminated = False
+    while terminated is False:
+        actions = np.random.uniform(low=-1, high=1, size=7)
+        state, reward, terminated, truncated, info = env.step(actions)
+    env.reset()
+
+fidelities = [transition[0] for transition in env.transition_history]
+
+with open(RESULTS_DIR + "two_qubit_fidelities_random_actions.csv", "w") as f:
+    writer = csv.writer(f)
+    writer.writerow(fidelities)
