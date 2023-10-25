730c68fd83451c5e8a98019be069c390988a8538
Collin Farquhar <farquhar13@gmail.com>
2023-10-25 12:38:10 -0600

    fix observation space

--- src/relaqs/environments/learn_abitrary_target_gate.py
+++ src/relaqs/environments/learn_abitrary_target_gate.py
@@ -6,16 +6,18 @@ class RandomGateEnvNoisy(GateSynthEnvRLlibHaarNoisy):
     @classmethod
     def get_default_env_config(cls):
         config_dict = super().get_default_env_config()
-        config_dict["observation_space_size"] = (config_dict["observation_space_size"] - 3) * 2
+        config_dict["observation_space_size"] = 67
         return config_dict
 
     def reset(self, *, seed=None, options=None):
-        starting_observeration, info = super().reset()
+        _, info = super().reset()
         U = RandomSU2().get_matrix()
         self.U_target = U
         self.superoperator_target = self.unitary_to_superoperator(U)
-        return starting_observeration, info
+        starting_observation = self.get_observation()
+        print(len(starting_observation))
+        return starting_observation, info
     
     def get_observation(self):
         observation = super().get_observation()
-        return np.append(observation,self.unitary_to_observation(self.superoperator_target))
+        return np.append(observation, self.unitary_to_observation(self.superoperator_target))
