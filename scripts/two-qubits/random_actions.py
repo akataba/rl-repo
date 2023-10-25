47b2c47f9edd0ba69757eb598acdad948bda6643
Collin Farquhar <farquhar13@gmail.com>
2023-10-24 21:34:13 -0600

    numerics for random actions for one and two qubit random actions

--- scripts/two-qubits/random_actions.py
+++ scripts/two-qubits/random_actions.py
@@ -1,20 +1,43 @@
-from relaqs.environments.gate_synth_env_rllib_Haar import TwoQubitGateSynth
+from relaqs.environments.gate_synth_env_rllib_Haar import TwoQubitGateSynth, GateSynthEnvRLlibHaarNoisy
 from relaqs import RESULTS_DIR
 import numpy as np
 import csv
 
-n_episodes = 100_000 
+def test_one_qubit():
+    n_episodes = 400_000 
 
-env = TwoQubitGateSynth(TwoQubitGateSynth.get_default_env_config())
-for _ in range(n_episodes):
-    terminated = False
-    while terminated is False:
-        actions = np.random.uniform(low=-1, high=1, size=7)
-        state, reward, terminated, truncated, info = env.step(actions)
-    env.reset()
+    env = GateSynthEnvRLlibHaarNoisy(GateSynthEnvRLlibHaarNoisy.get_default_env_config())
+    for _ in range(n_episodes):
+        terminated = False
+        while terminated is False:
+            actions = np.random.uniform(low=-1, high=1, size=2)
+            state, reward, terminated, truncated, info = env.step(actions)
+        env.reset()
 
-fidelities = [transition[0] for transition in env.transition_history]
+    fidelities = [transition[0] for transition in env.transition_history]
 
-with open(RESULTS_DIR + "two_qubit_fidelities_random_actions.csv", "w") as f:
-    writer = csv.writer(f)
-    writer.writerow(fidelities)
+    with open(RESULTS_DIR + "one_qubit_fidelities_random_actions.csv", "w") as f:
+        writer = csv.writer(f)
+        writer.writerow(fidelities)
+
+def test_two_qubits():
+    n_episodes = 100_000 
+
+    env = TwoQubitGateSynth(TwoQubitGateSynth.get_default_env_config())
+    for _ in range(n_episodes):
+        terminated = False
+        while terminated is False:
+            actions = np.random.uniform(low=-1, high=1, size=7)
+            state, reward, terminated, truncated, info = env.step(actions)
+        env.reset()
+
+    fidelities = [transition[0] for transition in env.transition_history]
+
+    with open(RESULTS_DIR + "two_qubit_fidelities_random_actions.csv", "w") as f:
+        writer = csv.writer(f)
+        writer.writerow(fidelities)
+
+if __name__ == "__main__":
+    test_one_qubit()
+    #test_two_qubits()
+    
