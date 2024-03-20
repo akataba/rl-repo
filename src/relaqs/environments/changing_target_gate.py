f8d6022107e6f07d3444121715c976ceeb0460aa
Collin Farquhar <farquhar13@gmail.com>
2024-03-19 21:31:24 -0600

    rename

---
+++ src/relaqs/environments/changing_target_gate.py
@@ -0,0 +1,22 @@
+import numpy as np
+from relaqs.environments import NoisySingleQubitEnv
+from relaqs.api.gates import RandomSU2
+
+class ChangingTargetEnv(NoisySingleQubitEnv):
+    @classmethod
+    def get_default_env_config(cls):
+        config_dict = super().get_default_env_config()
+        config_dict["observation_space_size"] = 67
+        return config_dict
+
+    def reset(self, *, seed=None, options=None):
+        _, info = super().reset()
+        U = RandomSU2().get_matrix()
+        self.U_target = self.unitary_to_superoperator(U)
+        starting_observation = self.get_observation()
+        print(len(starting_observation))
+        return starting_observation, info
+
+    def get_observation(self):
+        observation = super().get_observation()
+        return np.append(observation, self.unitary_to_observation(self.U_target))
