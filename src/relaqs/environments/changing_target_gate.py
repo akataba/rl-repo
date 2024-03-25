793528bfaec0922714f141f829f163fec46dacd9
Collin Farquhar <farquhar13@gmail.com>
2024-03-25 15:03:42 -0600

    set up script to learn X and H

--- src/relaqs/environments/changing_target_gate.py
+++ src/relaqs/environments/changing_target_gate.py
@@ -1,3 +1,4 @@
+import random
 import numpy as np
 from relaqs.environments import NoisySingleQubitEnv
 from relaqs.api.gates import RandomSU2
@@ -7,12 +8,23 @@ class ChangingTargetEnv(NoisySingleQubitEnv):
     def get_default_env_config(cls):
         config_dict = super().get_default_env_config()
         config_dict["observation_space_size"] = 67
+        config_dict["U_target_list"] = []
         return config_dict
+    
+    def __init__(self, env_config):
+        super().__init__(env_config)
+        self.U_target_list = env_config["U_target_list"]
+
+    def set_target_gate(self):
+        if len(self.U_target_list) == 0:
+            U = RandomSU2().get_matrix()
+        else:
+            U = random.choice(self.U_target_list)
+        self.U_target = self.unitary_to_superoperator(U)
 
     def reset(self, *, seed=None, options=None):
         _, info = super().reset()
-        U = RandomSU2().get_matrix()
-        self.U_target = self.unitary_to_superoperator(U)
+        self.set_target_gate()
         starting_observation = self.get_observation()
         return starting_observation, info
 
