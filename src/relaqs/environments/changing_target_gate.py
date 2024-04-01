6877d1705eb04ea013ebcdf54f9848e2d6ab4ebb
Collin Farquhar <farquhar13@gmail.com>
2024-04-01 12:07:36 -0600

    adding target_generation_function

--- src/relaqs/environments/changing_target_gate.py
+++ src/relaqs/environments/changing_target_gate.py
@@ -9,15 +9,17 @@ class ChangingTargetEnv(NoisySingleQubitEnv):
         config_dict = super().get_default_env_config()
         config_dict["observation_space_size"] = 67
         config_dict["U_target_list"] = []
+        config_dict["target_generation_function"] = RandomSU2
         return config_dict
     
     def __init__(self, env_config):
         super().__init__(env_config)
         self.U_target_list = env_config["U_target_list"]
+        self.target_generation_function = env_config["target_generation_function"]
 
     def set_target_gate(self):
         if len(self.U_target_list) == 0:
-            U = RandomSU2().get_matrix()
+            U = self.target_generation_function().get_matrix()
         else:
             U = random.choice(self.U_target_list)
         self.U_target = self.unitary_to_superoperator(U)
