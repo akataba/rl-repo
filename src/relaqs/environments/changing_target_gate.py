ca28b7571df4d0a00cb24ac330c223879c1ce3c6
Collin Farquhar <farquhar13@gmail.com>
2024-05-13 22:13:43 -0600

    separate environments for noiseless and noisy

--- src/relaqs/environments/changing_target_gate.py
+++ src/relaqs/environments/changing_target_gate.py
@@ -1,15 +1,42 @@
 import random
 import numpy as np
-from relaqs.environments import NoisySingleQubitEnv
+from relaqs.environments import SingleQubitEnv, NoisySingleQubitEnv
 from relaqs.api.gates import RandomSU2
 
-class ChangingTargetEnv(NoisySingleQubitEnv):
+class ChangingTargetEnv(SingleQubitEnv):
+    @classmethod
+    def get_default_env_config(cls):
+        config_dict = super().get_default_env_config()
+        config_dict["observation_space_size"] = 17
+        config_dict["target_generation_function"] = RandomSU2
+        return config_dict
+    
+    def __init__(self, env_config):
+        super().__init__(env_config)
+        self.U_target_list = env_config["U_target_list"]
+        self.target_generation_function = env_config["target_generation_function"]
+
+    def set_target_gate(self):
+        if len(self.U_target_list) == 0:
+            self.U = self.target_generation_function().get_matrix()
+        else:
+            self.U = random.choice(self.U_target_list)
+
+    def reset(self, *, seed=None, options=None):
+        _, info = super().reset()
+        self.set_target_gate()
+        starting_observation = self.get_observation()
+        return starting_observation, info
+
+    def get_observation(self):
+        observation = super().get_observation()
+        return np.append(observation, self.unitary_to_observation(self.U_target))
+    
+class NoisyChangingTargetEnv(ChangingTargetEnv, NoisySingleQubitEnv):
     @classmethod
     def get_default_env_config(cls):
         config_dict = super().get_default_env_config()
         config_dict["observation_space_size"] = 67
-        config_dict["U_target_list"] = []
-        config_dict["target_generation_function"] = RandomSU2
         return config_dict
     
     def __init__(self, env_config):
@@ -23,13 +50,3 @@ class ChangingTargetEnv(NoisySingleQubitEnv):
         else:
             U = random.choice(self.U_target_list)
         self.U_target = self.unitary_to_superoperator(U)
-
-    def reset(self, *, seed=None, options=None):
-        _, info = super().reset()
-        self.set_target_gate()
-        starting_observation = self.get_observation()
-        return starting_observation, info
-
-    def get_observation(self):
-        observation = super().get_observation()
-        return np.append(observation, self.unitary_to_observation(self.U_target))
