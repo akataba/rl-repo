3fd283ffb4d73f2bd198828ef1830e527e5a805e
Collin Farquhar <farquhar13@gmail.com>
2024-06-28 16:06:08 -0600

    rm TrainRLLib class and references

--- scripts/ddpg_on_env.py
+++
@@ -1,7 +0,0 @@
-from relaqs.api.training import TrainRLLib
-from relaqs.environments.gate_synth_env_rllib import GateSynthEnvRLlib
-from ray.rllib.algorithms.ddpg import DDPGConfig
-
-
-trainer = TrainRLLib(DDPGConfig, GateSynthEnvRLlib, episodes=100)
-trainer.train_model()
