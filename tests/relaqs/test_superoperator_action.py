3f8a7c19dfef7fb728735c6c4abdda21380ff513
Collin Farquhar <farquhar13@gmail.com>
2024-07-07 14:47:18 -0600

    4 ways of state evolution

--- tests/relaqs/test_superoperator_action.py
+++ tests/relaqs/test_superoperator_action.py
@@ -1,30 +1,27 @@
-from relaqs.environments import NoisySingleQubitEnv
 from relaqs.api.gates import RandomSU2
 from relaqs.api import gates
 from qiskit.quantum_info import random_statevector
 import numpy as np
-#from qiskit.quantum_info import SuperOp
-
-noisy_env = NoisySingleQubitEnv(NoisySingleQubitEnv.get_default_env_config())
 
 psi = random_statevector(2).data.reshape(-1, 1)
 dm = psi @ psi.conj().T
+U = RandomSU2().get_matrix()
 
-U = gates.X().get_matrix() # Gives np.allclose(U_dm, super_dm) = True
-#U = RandomSU2().get_matrix() # Gives np.allclose(U_dm, super_dm) = False
-
-# Regular matrix vector multiplication --> dm
+# State vector evolution
 U_psi = U @ psi
-U_dm = U_psi @ U_psi.conj().T
+dm1 = U_psi @ U_psi.conj().T
 
-# Superoperator evoluion
-U_super = noisy_env.unitary_to_superoperator(U)
+# Density matrix evolution
+dm2 = U @ dm @ U.conj().T
 
-# Potention fix
-U_super = np.kron(U, U.conj())
+# Superoperator evolution, with row-order vectorization
+superop1 = np.kron(U, U.conj())
+dm3 = (superop1 @ dm.reshape(-1, 1)).reshape(2, 2)
 
-super_dm = (U_super @ dm.reshape(-1, 1)).reshape(2, 2)
+# Superoperator evolution, with column-order vectorization
+superop2 = np.kron(U.conj(), U)
+dm4 = (superop2 @ dm.reshape(-1, 1, order="F")).reshape(2, 2, order="F")
 
-print(U_dm)
-print(super_dm)
-print(np.allclose(U_dm, super_dm))
+print(np.allclose(dm1, dm2))
+print(np.allclose(dm2, dm3))
+print(np.allclose(dm3, dm4))
