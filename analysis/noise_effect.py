43cce360504905415bae7a9f1f90c72e1e573b7a
Collin Farquhar <farquhar13@gmail.com>
2024-07-08 16:18:45 -0600

    zero noise vs sample noise options

--- analysis/noise_effect.py
+++ analysis/noise_effect.py
@@ -3,19 +3,23 @@ from relaqs.api.utils import sample_noise_parameters
 from relaqs.environments import SingleQubitEnv, NoisySingleQubitEnv
 from relaqs.api.gates import RandomSU2
 from qutip.operators import sigmaz, sigmam
-from qiskit.quantum_info import random_statevector, random_unitary  
+
+zero_noise = True
 
 env_config = NoisySingleQubitEnv.get_default_env_config()
 
-# Test with noise
-env_config["relaxation_rates_list"] = [np.reciprocal(t1_list).tolist(), np.reciprocal(t2_list).tolist()]
-#env_config["delta"] = [10e10]
-#env_config["relaxation_ops"] = [sigmam(), sigmaz()]
+if zero_noise:
+    env_config["relaxation_rates_list"] = []
+    env_config["relaxation_ops"] = []
+    env_config["detuning_list"] = [0]
+else: # sample noise
+    # Test with IBM T1 and T2 data and sampling detuning from a normal distribution
+    noise_file = "april/ibmq_belem_month_is_4.json"
+    t1_list, t2_list, detuning_list = sample_noise_parameters(noise_file)
 
-# zeroing out relaxation rates
-#env_config["relaxation_rates_list"] = []
-env_config["relaxation_ops"] = []
-env_config["detuning_list"] = [0]
+    env_config["relaxation_rates_list"] = [np.reciprocal(t1_list).tolist(), np.reciprocal(t2_list).tolist()]
+    env_config["detuning_list"] = detuning_list
+    env_config["relaxation_ops"] = [sigmam(), sigmaz()]
 
 noiseless_env = SingleQubitEnv(SingleQubitEnv.get_default_env_config())
 noisy_env = NoisySingleQubitEnv(env_config)
@@ -37,5 +41,9 @@ noiseless_superop = NoisySingleQubitEnv.unitary_to_superoperator(noiseless_U)
 
 print("super op close after step:", np.allclose(noiseless_superop, noisy_superop))
 
-noiseless_fidelity = noiseless_env.compute_fidelity()
 noisy_fidelity = noisy_env.compute_fidelity()
+noisy_env.U = noiseless_superop
+noiseless_fidelity = noisy_env.compute_fidelity()
+
+superop_fidelity_difference = noiseless_fidelity - noisy_fidelity
+print("Superoperator fidelitiy difference:", superop_fidelity_difference)
