caac53059d52cf51b717d9b18f2cfef9881a9dc0
Collin Farquhar <farquhar13@gmail.com>
2024-07-09 10:41:57 -0600

    cleanup

--- analysis/noise_effect.py
+++ analysis/noise_effect.py
@@ -11,10 +11,11 @@ from relaqs.environments import SingleQubitEnv, NoisySingleQubitEnv
 from relaqs.api.gates import RandomSU2
 from qutip.operators import sigmaz, sigmam
 
-zero_noise = True
+zero_noise = False
 
+# Define environments
+noiseless_env = SingleQubitEnv(SingleQubitEnv.get_default_env_config())
 env_config = NoisySingleQubitEnv.get_default_env_config()
-
 if zero_noise:
     env_config["relaxation_rates_list"] = []
     env_config["relaxation_ops"] = []
@@ -27,30 +28,29 @@ else: # sample noise
     env_config["relaxation_rates_list"] = [np.reciprocal(t1_list).tolist(), np.reciprocal(t2_list).tolist()]
     env_config["detuning_list"] = detuning_list
     env_config["relaxation_ops"] = [sigmam(), sigmaz()]
-
-noiseless_env = SingleQubitEnv(SingleQubitEnv.get_default_env_config())
 noisy_env = NoisySingleQubitEnv(env_config)
 
+# Set random U_target
 U_target = RandomSU2().get_matrix()
 noiseless_env.U_target = U_target
 noisy_env.U_target = NoisySingleQubitEnv.unitary_to_superoperator(U_target)
 
-noiseless_fidelity = noiseless_env.compute_fidelity()
-noisy_fidelity = noisy_env.compute_fidelity()
-
+# Take random actions
 actions = noiseless_env.action_space.sample()
 noiseless_env.step(actions)
 noisy_env.step(actions)
-noiseless_U = noiseless_env.U
-noisy_superop = noisy_env.U
 
-noiseless_superop = NoisySingleQubitEnv.unitary_to_superoperator(noiseless_U)
+# Get superoperators
+noiseless_superop = NoisySingleQubitEnv.unitary_to_superoperator(noiseless_env.U)
+noisy_superop = noisy_env.U
 
 print("super op close after step:", np.allclose(noiseless_superop, noisy_superop))
 
+# Compute superoperator fidelities
 noisy_fidelity = noisy_env.compute_fidelity()
 noisy_env.U = noiseless_superop
 noiseless_fidelity = noisy_env.compute_fidelity()
 
+# Compute superoperator fidelity difference
 superop_fidelity_difference = noiseless_fidelity - noisy_fidelity
 print("Superoperator fidelitiy difference:", superop_fidelity_difference)
