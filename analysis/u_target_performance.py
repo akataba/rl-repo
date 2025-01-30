d99e07f41caba779e3d257ce84d72921d20c5ea5
Farquhar13 <farquhar13@gmail.com>
2025-01-29 17:30:19 -0700

    last n fix

--- analysis/u_target_performance.py
+++ analysis/u_target_performance.py
@@ -7,20 +7,20 @@ df = load_pickled_env_data(data_path)
 
 last_n = 1000
 
-fidelity = df["Fidelity"][:last_n]
-reward = df["Rewards"][:last_n]
-actions = df["Actions"][:last_n]
-operator = df["Operator"][:last_n]
-target_operator = df["Target Operator"][:last_n]
-episode_id = df["Episode Id"][:last_n]
+fidelity = df["Fidelity"].tail(last_n)
+reward = df["Rewards"].tail(last_n)
+actions = df["Actions"].tail(last_n)
+operator = df["Operator"].tail(last_n)
+target_operator = df["Target Operator"].tail(last_n)
+episode_id = df["Episode Id"].tail(last_n)
 
 n_lowest = 10
-
-lowest_fidelities = fidelity.nsmallest(n_lowest).values
-lowest_fidelities_indices = fidelity.nsmallest(n_lowest).index
-lowest_fidelity_target_operators = target_operator.iloc[lowest_fidelities_indices]
+fidelities_np = fidelity.to_numpy()
+lowest_fidelities = np.partition(fidelities_np, n_lowest)[:n_lowest]
+lowest_fidelities_indices = np.argsort(fidelities_np)[:n_lowest]
+lowest_fidelity_target_operators = target_operator.to_numpy()[lowest_fidelities_indices]
 
 print("Target operators corresponding to the worst fidelity:")
 for i in range(n_lowest):
     print("Fidelity:", lowest_fidelities[i])
-    print(lowest_fidelity_target_operators.iloc[i], "\n")
+    print(lowest_fidelity_target_operators[i], "\n")
