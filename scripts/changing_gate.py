e54e4123258bc68122cec3229fb9ce0ab6d30a6c
Farquhar13 <farquhar13@gmail.com>
2025-02-24 20:40:16 -0700

    title fix

--- scripts/changing_gate.py
+++ scripts/changing_gate.py
@@ -42,9 +42,10 @@ def run(env=ChangingTargetEnv, n_training_iterations=1, save=True, plot=True):
 
     alg_config.actor_hidden_activation = "relu"
     alg_config.critic_hidden_activation = "relu"
-    alg_config.num_steps_sampled_before_learning_starts = 1000
-    alg_config.actor_hiddens = [30,30,30]
-    alg_config.exploration_config["scale_timesteps"] = 10000
+    alg_config.num_steps_sampled_before_learning_starts = 100
+    alg_config.actor_hiddens = [300,300,300, 300, 300]
+    alg_config.exploration_config["scale_timesteps"] = 1000
+    alg_config.train_batch_size = 256
 
     alg = alg_config.build()
     # ---------------------------------------------------------------------
@@ -62,14 +63,14 @@ def run(env=ChangingTargetEnv, n_training_iterations=1, save=True, plot=True):
     # ---------------------> Plot Data <-------------------------
     if plot is True:
         assert save is True, "If plot=True, then save must also be set to True"
-        env_string = "Noiseless" if isinstance(env, ChangingTargetEnv) else "Noisy"
-        plot_data(save_dir, episode_length=alg._episode_history[0].episode_length, figure_title=env_string +" Random SU(2)")
+        env_string = "Noisy" if isinstance(env, NoisyChangingTargetEnv) else "Noiseless"
+        plot_data(save_dir, episode_length=alg._episode_history[0].episode_length, figure_title=env_string +" Random SU(2), bigger network, batch=256 ")
         print("Plots Created")
     # --------------------------------------------------------------
 
 if __name__ == "__main__":
-    env = ChangingTargetEnv
-    n_training_iterations = 1
+    env = NoisyChangingTargetEnv
+    n_training_iterations = 100
     save = True
     plot = True
     run(env, n_training_iterations, save, plot)
